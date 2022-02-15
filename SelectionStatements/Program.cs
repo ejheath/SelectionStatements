d7633e5e6d6de24dddc54e1b9eb1d0d14d328e60
Elijah Heath <ejheath8888@gmail.com>
2022-02-15 11:08:28 -0500

    if-else in practice

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            int favoriteNumber = 8;
+            int guess;
+            Console.WriteLine("Welcome to the guessing game!");
+
+            Console.WriteLine("Please enter a number between 1 - 1000: ");
+
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess < favoriteNumber)
+            {
+                Console.WriteLine("Your guess is too low. Try once more.");
+            } else if (guess > favoriteNumber)
+            {
+                Console.WriteLine("Your guess is too high! Try again.");
+            } else
+            {
+                Console.WriteLine("Correct!");
+            }
+
+        }
+    }
+}
